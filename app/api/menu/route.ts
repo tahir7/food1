be09ff52fcb75dd80c9f7f889ba3819202c0c2bc
Tahir Abbas <iamtahirabbas@gmail.com>
2025-06-18 13:01:19 -0400

    Removed removed files-prisma

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -1,50 +1,50 @@
-import { NextRequest, NextResponse } from 'next/server';
-import React from 'react';
-import  {schema}  from "./schema";
-import {prisma} from  "@/prisma/client"
+// import { NextRequest, NextResponse } from 'next/server';
+// import React from 'react';
+// import  {schema}  from "./schema";
 
 
-interface data {
-    dishname : string,
-    price : number,
-    // size  : number
-}
-export  async function POST(request : NextRequest) {
+
+// interface data {
+//     dishname : string,
+//     price : number,
+//     // size  : number
+// }
+// export  async function POST(request : NextRequest) {
 
     
-    const body : data = await request.json();
+//     const body : data = await request.json();
 
-    console.log(body);
+//     console.log(body);
 
-    const validate = schema.safeParse(body);
+//     const validate = schema.safeParse(body);
 
-    if(!validate.success) {
-        return NextResponse.json(validate)
-    }
+//     if(!validate.success) {
+//         return NextResponse.json(validate)
+//     }
 
-    const newItem = await prisma.menu.create ( {
+//     const newItem = await prisma.menu.create ( {
         
-        data : {
-            dishname : body.dishname,
-            price : body.price
+//         data : {
+//             dishname : body.dishname,
+//             price : body.price
            
-        }
-    })
+//         }
+//     })
     
-    return  NextResponse.json(body);
+//     return  NextResponse.json(body);
     
-}
+// }
 
-export async function  GET(request : NextRequest) {
+// export async function  GET(request : NextRequest) {
     
-    const resp = await prisma.menu.findMany()
+//     const resp = await prisma.menu.findMany()
 
-    return NextResponse.json("A")
-    // return NextResponse.json(resp);
+//     return NextResponse.json("A")
+//     // return NextResponse.json(resp);
 
-    // return resp.map(i=>
-    // {
-    //     i.dishname + "<br>"
-    // }
-    // )
-}
+//     // return resp.map(i=>
+//     // {
+//     //     i.dishname + "<br>"
+//     // }
+//     // )
+// }
